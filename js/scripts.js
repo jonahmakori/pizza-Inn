1e0a93e45b971079d1b9d22f6f5579a108bdb5ac
Jonah <jonahmakori0@gmail.com>
2019-12-16 16:16:08 +0300

    modified js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 function getSize() {
-    var size = document.getElementById("sel1").value;
+    var size = document.getElementById("size").value;
     return parseInt(size);
 }
 
 function getFlavour() {
-    var flavour = document.getElementById("sel2").value;
+    var flavour = document.getElementById("crust").value;
     return parseInt(flavour);
 }
 
 function getTopping() {
-    var topping = document.getElementById("sel3").value;
+    var topping = document.getElementById("toppings").value;
     return parseInt(topping);
 }
 
@@ -23,11 +23,11 @@ function calculateTotal() {
 
     alert("You have ordered " + getNumber("") + " Pizza " + " which amounts to Kshs. " + results + " Thank you ");
     prompt("Enter your location");
-    alert("Your order has been received and will be delivered in a short while. Delivery fee is kshs 150.");
+    alert("Your order has been placed, It will be delivered in a short while. The delivery fee is kshs 170.");
 }
 function calculate() {
     var results = (getSize() + getFlavour() + getTopping()) * getNumber();
 
-    alert("You have ordered " + getNumber("") + " Pizza " + " which amounts to Kshs. " + results + " Your order is ready for pickup at our station along the University Way. Contact us for more information. Thank you.");
+    alert("You have ordered " + getNumber("") + " Pizza " + " which amounts to Kshs. " + results + " Your order is ready for pickup at our station along Gong lane plaza.Feel free to  Contact us for more information.See you soon.");
 
 }
