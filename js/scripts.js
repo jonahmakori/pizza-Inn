07fdd4ed4104082e67a12e06157fbe16400720c5
Jonah <jonahmakori0@gmail.com>
2019-12-20 08:30:37 +0300

    Used constructors and prototypes in the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,49 @@
-function getSize() {
-    var size = document.getElementById("size").value;
-    return parseInt(size);
+function PizzaOrder(psize, pflavour, ptopping, pnumber) {
+  this.size = psize;
+  this.flavour = pflavour;
+  this.toppings = ptopping;
+  this.number = pnumber;
 }
+PizzaOrder.prototype.calculateTotal = function() {
+  var total = (this.size + this.flavour + this.toppings) * this.number;
+  return total;
+};
 
-function getFlavour() {
-    var flavour = document.getElementById("crust").value;
-    return parseInt(flavour);
+function orderDelivery(delivery = 170) {
+  var size, flavour, topping, number;
+  size = parseInt($("#size").val());
+  flavour = parseInt($("#size").val());
+  topping = parseInt($("#toppings").val());
+  number = parseInt($("#number").val());
+
+  var order = new PizzaOrder(size, flavour, topping, number);
+  alert(
+    "You have ordered " +
+      order.number +
+      " Pizza " +
+      " which amounts to Kshs. " +
+      (order.calculateTotal() + delivery) +
+      " Thank you "
+  );
+  prompt("Enter your location");
+  alert(
+    `Your order has been placed, It will be delivered in a short while. The delivery fee is kshs ${delivery}.`
+  );
 }
+function orderPickup() {
+  var size, flavour, topping, number;
+  size = parseInt($("#size").val());
+  flavour = parseInt($("#size").val());
+  topping = parseInt($("#toppings").val());
+  number = parseInt($("#number").val());
 
-function getTopping() {
-    var topping = document.getElementById("toppings").value;
-    return parseInt(topping);
-}
-
-function getNumber() {
-    var number = document.getElementById("number").value;
-    return parseInt(number);
-}
-
-function calculateTotal() {
-    var results = (getSize() + getFlavour() + getTopping()) * getNumber();
-
-    alert("You have ordered " + getNumber("") + " Pizza " + " which amounts to Kshs. " + results + " Thank you ");
-    prompt("Enter your location");
-    alert("Your order has been placed, It will be delivered in a short while. The delivery fee is kshs 170.");
-}
-function calculate() {
-    var results = (getSize() + getFlavour() + getTopping()) * getNumber();
-
-    alert("You have ordered " + getNumber("") + " Pizza " + " which amounts to Kshs. " + results + " Your order is ready for pickup at our station along Gong lane plaza.Feel free to  Contact us for more information.See you soon.");
-
+  var order = new PizzaOrder(size, flavour, topping, number);
+  alert(
+    "You have ordered " +
+      order.number +
+      " Pizza " +
+      " which amounts to Kshs. " +
+      order.calculateTotal() +
+      " Your order is ready for pickup at our station along Gong lane plaza.Feel free to  Contact us for more information.See you soon."
+  );
 }
